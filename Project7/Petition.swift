978653330870aaed29cb6247cf5ddf4b01d4d419
zug <dungnguyen.ttcb@gmail.com>
2022-10-26 00:06:39 +0700

    Completed app. But Xcode 12.4, it doesn't work

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by ZuG on 10/25/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: String
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by ZuG on 10/25/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
